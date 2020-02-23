318ae31911991f590e0a9d96c14667ba12f3b009
Ashish Patel <toaashishpatel@gmail.com>
2020-02-24 02:04:38 +0530

    Added SQS Configuration and app settings

---
+++ src/AwsSQSLabs.Web/Configuration/SQSConfig.cs
@@ -0,0 +1,12 @@
+﻿#region Imports
+#endregion
+
+namespace AwsSQSLabs.Web.Configuration
+{
+    public class SQSConfig
+    {
+        public string AWSRegion { get; set; }
+
+        public string QueueUrl { get; set; }
+    }
+}
