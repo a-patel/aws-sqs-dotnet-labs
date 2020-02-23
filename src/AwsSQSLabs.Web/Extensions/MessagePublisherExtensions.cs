e4590c0d36a4dead16898c430810cda1462a1161
Ashish Patel <toaashishpatel@gmail.com>
2020-02-24 02:06:55 +0530

    Temp commit - Message Publisher

---
+++ src/AwsSQSLabs.Web/Extensions/MessagePublisherExtensions.cs
@@ -0,0 +1,16 @@
+﻿//#region Imports
+//using AwsSQSLabs.Web.Messaging;
+//using System;
+//using System.Threading.Tasks;
+//#endregion
+
+//namespace AwsSQSLabs.Web.Extensions
+//{
+//    public static class MessagePublisherExtensions
+//    {
+//        public static Task PublishAsync<T>(this IMessagePublisher publisher, T message, TimeSpan? delay = null) where T : class
+//        {
+//            return publisher.PublishAsync(typeof(T), message, delay);
+//        }
+//    }
+//}
---
+++ src/AwsSQSLabs.Web/Messaging/IMessagePublisher.cs
@@ -0,0 +1,13 @@
+﻿//#region Imports
+//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+//#endregion
+
+//namespace AwsSQSLabs.Web.Messaging
+//{
+//    public interface IMessagePublisher
+//    {
+//        Task PublishAsync(Type messageType, object message, TimeSpan? delay = null, CancellationToken cancellationToken = default);
+//    }
+//}
