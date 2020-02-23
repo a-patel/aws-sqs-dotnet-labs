6d52dcedebff2a340545783a2731773cc28c3f06
Ashish Patel <toaashishpatel@gmail.com>
2020-02-24 02:07:18 +0530

    Temp commit - Message Subscriber

---
+++ src/AwsSQSLabs.Web/Extensions/MessageSubscriberExtensions.cs
@@ -0,0 +1,45 @@
+﻿//#region Imports
+//using AwsSQSLabs.Web.Messaging;
+//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+//#endregion
+
+//namespace AwsSQSLabs.Web.Extensions
+//{
+//    public static class MessageSubscriberExtensions
+//    {
+//        public static Task SubscribeAsync<T>(this IMessageSubscriber subscriber, Func<T, Task> handler, CancellationToken cancellationToken = default) where T : class
+//        {
+//            return subscriber.SubscribeAsync<T>((msg, token) => handler(msg), cancellationToken);
+//        }
+
+//        public static Task SubscribeAsync<T>(this IMessageSubscriber subscriber, Action<T> handler, CancellationToken cancellationToken = default) where T : class
+//        {
+//            return subscriber.SubscribeAsync<T>((msg, token) =>
+//            {
+//                handler(msg);
+//                return Task.CompletedTask;
+//            }, cancellationToken);
+//        }
+
+//        public static Task SubscribeAsync(this IMessageSubscriber subscriber, Func<IMessage, CancellationToken, Task> handler, CancellationToken cancellationToken = default)
+//        {
+//            return subscriber.SubscribeAsync<IMessage>((msg, token) => handler(msg, token), cancellationToken);
+//        }
+
+//        public static Task SubscribeAsync(this IMessageSubscriber subscriber, Func<IMessage, Task> handler, CancellationToken cancellationToken = default)
+//        {
+//            return subscriber.SubscribeAsync((msg, token) => handler(msg), cancellationToken);
+//        }
+
+//        public static Task SubscribeAsync(this IMessageSubscriber subscriber, Action<IMessage> handler, CancellationToken cancellationToken = default)
+//        {
+//            return subscriber.SubscribeAsync((msg, token) =>
+//            {
+//                handler(msg);
+//                return Task.CompletedTask;
+//            }, cancellationToken);
+//        }
+//    }
+//}
---
+++ src/AwsSQSLabs.Web/Messaging/IMessageSubscriber.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace AwsSQSLabs.Web.Messaging
+//{
+//    public interface IMessageSubscriber
+//    {
+//        Task SubscribeAsync<T>(Func<T, CancellationToken, Task> handler, CancellationToken cancellationToken = default) where T : class;
+//    }
+
+//    public static class MessageBusExtensions
+//    {
+//    }
+//}
