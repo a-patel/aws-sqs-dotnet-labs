2ba2cfb60fcd63f38ddbce308776ed21c031c3ca
Ashish Patel <toaashishpatel@gmail.com>
2021-10-16 23:18:37 +0530

    added usage in sample controller

---
+++ src/AwsSQSLabs.Web/Controllers/MovieController.cs
@@ -0,0 +1,45 @@
+﻿#region Imports
+using AwsSQSLabs.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using AwsSQSLabs.Web.Models;
+#endregion
+
+namespace AwsSQSLabs.Web.Controllers
+{
+    public class MovieController : ControllerBase
+    {
+        #region Members
+
+        private readonly IMovieService _movieService;
+
+        #endregion
+
+        #region Ctor
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReleaseMovie([FromBody] MovieModel model)
+        {
+            await _movieService.PublishMovie(model);
+
+            return Ok();
+        }
+
+        #endregion
+    }
+}
